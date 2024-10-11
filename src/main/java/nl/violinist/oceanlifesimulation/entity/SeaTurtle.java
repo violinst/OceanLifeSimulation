94c5092168bad465a2ec4491cc187fe1641f50a8
violinist <vioolstudiorotterdam@gmail.com>
2024-10-11 10:45:34 +0200

    SeaTurtle fix alignment

--- src/main/java/nl/violinist/oceanlifesimulation/entity/SeaTurtle.java
+++ src/main/java/nl/violinist/oceanlifesimulation/entity/SeaTurtle.java
@@ -6,18 +6,22 @@ public class SeaTurtle {
     private int energy = 100;
     private final double jawDigestionCoefficient = 3.2;
 
-    public void setHealth (int health) {
+    public void setHealth(int health) {
         this.health = health;
     }
+
     public int getHealth() {
         return this.health;
     }
+
     public void setEnergy(int energy) {
         this.energy = energy;
     }
+
     public int getEnergy() {
         return this.energy;
     }
+
     public double getJawDigestionCoefficient() {
         return this.jawDigestionCoefficient;
     }
