6c85c99a48bf4bb647bd3f795fd95cf872a9df40
violinist <vioolstudiorotterdam@gmail.com>
2024-10-11 10:41:22 +0200

    SeaTurtle add method setHealth

--- src/main/java/nl/violinist/oceanlifesimulation/entity/SeaTurtle.java
+++ src/main/java/nl/violinist/oceanlifesimulation/entity/SeaTurtle.java
@@ -1,4 +1,12 @@
 package nl.violinist.oceanlifesimulation.entity;
 
 public class SeaTurtle {
+
+    private int health = 100;
+    private int energy = 100;
+    private final double jawDigestionCoefficient = 3.2;
+
+    public void setHealth (int health) {
+        this.health = health;
+    }
 }
