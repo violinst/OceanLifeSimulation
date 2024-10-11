4c9142ebffc0f07f416121a257f7eb3ce90037be
violinist <vioolstudiorotterdam@gmail.com>
2024-10-11 12:14:50 +0200

    LifeEventsSimulator create method sleepSimulation + change name startSimulation

--- src/main/java/nl/violinist/oceanlifesimulation/util/LifeEventsSimulator.java
+++ src/main/java/nl/violinist/oceanlifesimulation/util/LifeEventsSimulator.java
@@ -4,8 +4,17 @@ import nl.violinist.oceanlifesimulation.entity.SeaTurtle;
 
 public class LifeEventsSimulator {
 
-    public void startAction (SeaTurtle seaTurtle) {
+    public void startSimulation (SeaTurtle seaTurtle) {
 
+    }
+    private void sleepSimulation(SeaTurtle seaTurtle) {
+        int energy = seaTurtle.getEnergy();
+        energy = energy + 20;
+        if (energy > 100) {
+            energy = 100;
+        }
+        seaTurtle.setEnergy(energy);
+        System.out.println("Sea Turtle slept!");
     }
 
 }
