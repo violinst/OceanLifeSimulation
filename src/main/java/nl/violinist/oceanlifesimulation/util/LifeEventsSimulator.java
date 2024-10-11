ebe62ecc8cedf021960358be985a0b1349714f36
violinist <vioolstudiorotterdam@gmail.com>
2024-10-11 12:11:44 +0200

    LifeEventsSimulator add method public void startAction

--- src/main/java/nl/violinist/oceanlifesimulation/util/LifeEventsSimulator.java
+++ src/main/java/nl/violinist/oceanlifesimulation/util/LifeEventsSimulator.java
@@ -1,4 +1,11 @@
 package nl.violinist.oceanlifesimulation.util;
 
+import nl.violinist.oceanlifesimulation.entity.SeaTurtle;
+
 public class LifeEventsSimulator {
+
+    public void startAction (SeaTurtle seaTurtle) {
+
+    }
+
 }
